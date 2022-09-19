5fd6b0de5cef6bdf6805658ad259232131f8db8b
aitaddiadil <113940875+aitaddiadil@users.noreply.github.com>
2022-09-19 20:44:29 +0100

    Add files via upload

---
+++ unconstr/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head><title><?php echo "Coming Soon...";?></title>
+    </head>
+    <style>
+        .mainImg{
+            display:block;
+            margin-left:auto;
+            margin-right:auto;
+            pointer-events:none;
+            user-select:none;  
+        }
+    </style>
+    <body>
+        <?php 
+        $image_url='/uconstr/images/img1.png';
+        ?>
+        <div>
+            <br>
+            <img src="<?php echo $image_url;?>" width="1135" height="655" alt="under construction" class="mainImg" unselectable="on" >
+        </div>
+    </body>
+</html>
